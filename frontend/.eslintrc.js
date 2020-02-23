c5028c43781ea1ef8c87d59cdbbfd982614f2346
CanadianCommander <ben.benetti@shaw.ca>
2020-02-23 14:43:55 -0800

    Setup Vue, ESLint, ect.

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,43 @@
+module.exports = {
+	'env': {
+		'browser': true,
+		'es6': true,
+		'commonjs': true
+	},
+	'extends': [
+		'eslint:recommended',
+		'plugin:vue/essential',
+		'plugin:@typescript-eslint/eslint-recommended'
+	],
+	'globals': {
+		'Atomics': 'readonly',
+		'SharedArrayBuffer': 'readonly'
+	},
+	'parserOptions': {
+		'ecmaVersion': 2018,
+		'parser': '@typescript-eslint/parser',
+		'sourceType': 'module'
+	},
+	'plugins': [
+		'vue',
+		'@typescript-eslint'
+	],
+	'rules': {
+		'indent': [
+			'error',
+			'tab'
+		],
+		'linebreak-style': [
+			'error',
+			'unix'
+		],
+		'quotes': [
+			'error',
+			'single'
+		],
+		'semi': [
+			'error',
+			'always'
+		]
+	}
+};
